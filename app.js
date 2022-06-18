87052fdafe8641d2656b984040bc98e3c9ae3f2f
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-18 16:07:56 +0300

    innerHTML update

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-const Tİme = new Date();
-console.log(Time);
+const hours = document.querySelector(".hours");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const amPm = document.querySelector(".amPm");
+
+const getTime = () => {
+  hours.innerHTML = new Date().getHours() % 12;
+  if (new Date().getHours() > 12) {
+    amPm.textContent = "PM";
+  } else {
+    amPm.textContent = "AM";
+  }
+  minute.innerHTML = new Date().getMinutes();
+  second.innerHTML = new Date().getSeconds();
+};
+setInterval(getTime, 999);

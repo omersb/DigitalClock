6cafa596c14507be2994053dfaab373f43ce2f18
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-18 16:16:31 +0300

    date added

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const hours = document.querySelector(".hours");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 const amPm = document.querySelector(".amPm");
+const date = document.querySelector(".date");
 
 const getTime = () => {
   hours.innerHTML = new Date().getHours() % 12;
+  minute.innerHTML = new Date().getMinutes();
+  second.innerHTML = new Date().getSeconds();
+  date.innerHTML = new Date().getDate();
+
   if (new Date().getHours() > 12) {
     amPm.textContent = "PM";
   } else {
     amPm.textContent = "AM";
   }
-  minute.innerHTML = new Date().getMinutes();
-  second.innerHTML = new Date().getSeconds();
+  if (hours.textContent.length == 1) {
+    hours.textContent = "0" + hours.textContent;
+  }
+  if (minute.textContent.length == 1) {
+    minute.textContent = "0" + minute.textContent;
+  }
+  if (second.textContent.length == 1) {
+    second.textContent = "0" + second.textContent;
+  }
 };
 setInterval(getTime, 999);
